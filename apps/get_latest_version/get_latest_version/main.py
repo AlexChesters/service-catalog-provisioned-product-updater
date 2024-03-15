a59daa9eb5e324a0aa608090c88758df3350a0c2
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2024-03-15 12:01:55 +0000

    return some info

--- apps/get_latest_version/get_latest_version/main.py
+++ apps/get_latest_version/get_latest_version/main.py
@@ -1,7 +1,13 @@
 from aws_lambda_powertools import Logger
+import boto3
 
 logger = Logger()
+service_catalog = boto3.client("servicecatalog")
 
 @logger.inject_lambda_context(log_event=True)
-def handler(_event, _context):
-    pass
+def handler(event, _context):
+    response = service_catalog.describe_product(Name=event["product_name"])
+
+    for artifact in response["ProvisioningArtifacts"]:
+        if artifact["Guidance"] == "DEFAULT":
+            return artifact
